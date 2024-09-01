14cd7138f33c2e099737e881eba06455d7239624
Dmitriy <dm.tsiganov@gmail.com>
2024-09-01 23:50:21 +0700

    1 Задача

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int [] weight = new int [3];
+        weight[0] = 1;
+        weight[1] = 2;
+        weight[2] = 3;
+
+        double [] weight1 = {1.57, 7.654, 9.986};
+
+        int [] weight2 = {9, 1, 3, 2, 5, 7, 4, 8, 70, 10, 20, 10};
+
+
+
+
+    }
+}
