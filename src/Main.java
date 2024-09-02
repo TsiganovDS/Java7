edf4a5a0f17c74de121501501a2f0865172eaa69
Dmitriy <dm.tsiganov@gmail.com>
2024-09-02 17:17:39 +0700

    2 Задача

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,34 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int [] weight = new int [3];
+        int[] weight = new int[3];
         weight[0] = 1;
         weight[1] = 2;
         weight[2] = 3;
+        for (int i = 0; i < weight.length; i++) {
+            System.out.print(weight[i]);
+            if (i < weight.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
 
-        double [] weight1 = {1.57, 7.654, 9.986};
+        double[] weight1 = {1.57, 7.654, 9.986};
+        for (int i = 0; i < weight1.length; i++) {
+            System.out.print(weight1[i]);
+            if (i < weight1.length - 1) {
+                System.out.print(", ");
+            }
+        }
 
-        int [] weight2 = {9, 1, 3, 2, 5, 7, 4, 8, 70, 10, 20, 10};
+        System.out.println();
 
-
-
-
+        int[] weight2 = {9999, 1, 3333, 2, 555555, 7, 400009999, 8, 7, 1, 2, 10, 2, 1, 2, 3, 123, 124, 125};
+        for (int i = 0; i < weight2.length; i++) {
+            System.out.print(weight2[i]);
+            if (i < weight2.length - 1) {
+                System.out.print(", ");
+            }
+        }
     }
 }
